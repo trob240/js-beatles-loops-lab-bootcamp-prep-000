430f43352927e5c41839d84ef33faf58d30aa8fa
Trevahr Robertson <trevahr@yahoo.com>
2019-03-14 15:49:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+var musicians = [];
+var instruments = [];
+var newArray = [];
+
+
+function theBeatlesPlay(musicians, instruments){
+  var m = musicians.length - 1;
+  for (var i = 0; i <= m; i++)
+  
+  {
+  
+  newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+   }
+   return newArray;
+}
+
+
+function johnLennonFacts(facts) {
+  var newArray1 = [];
+  var i = 0;
+  var fl = facts.length -1;
+  while (facts.length > 0 && facts.length <= fl){
+     newArray1.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return newArray1;
+}
